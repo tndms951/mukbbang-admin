daa04cd14f735d5bb5aa14e04290c02dec3ceb35
송지수 <tndms951@naver.com>
2021-01-13 20:10:47 +0900

    delete console.log add PropTypes

--- src/components/admin-main/contents/bread-list/bread_router.jsx
+++ src/components/admin-main/contents/bread-list/bread_router.jsx
@@ -5,16 +5,13 @@ import BreadList from './bread_list';
 import BreadRegister from './bread-register/bread_register';
 import BreadDetail from './bread-detail/bread_detail';
 
-const BreadRouter = ({ match }) => {
-  console.log('aaa');
-  return (
-    <>
-      <Switch>
-        <Route path={`${match.path}/bread_register`} component={BreadRegister} />
-        <Route path={`${match.path}/detail/:breadId`} component={BreadDetail} />
-        <Route exact path={match.path} component={BreadList} />
-      </Switch>
-    </>
-  );
-};
+const BreadRouter = ({ match }) => (
+  <>
+    <Switch>
+      <Route path={`${match.path}/bread_register`} component={BreadRegister} />
+      <Route path={`${match.path}/detail/:breadId`} component={BreadDetail} />
+      <Route exact path={match.path} component={BreadList} />
+    </Switch>
+  </>
+);
 export default BreadRouter;
